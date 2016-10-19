92e9ecb1a9c3d52642b5406276a7f1d26a02a07a
joetsoi <joetsoi@users.noreply.github.com>
2016-10-19 01:08:39 +0100

    fix formatting

--- cpp/uncompress.cpp
+++ cpp/uncompress.cpp
@@ -13,64 +13,64 @@ void swap_endianness(T *objp) {
 
 
 std::vector<unsigned char> extractFile(std::ifstream& file) {
-	std::vector<unsigned char> extracted;
-	unsigned short file_type, file_size;
+    std::vector<unsigned char> extracted;
+    unsigned short file_type, file_size;
 
-	if(file){
-		file.read(reinterpret_cast<char*>(&file_type), 2);
-		swap_endianness(&file_type);
-		//std::cout << file_type << std::endl;
-		file.seekg(2, std::ios::cur);
-		file.read(reinterpret_cast<char*>(&file_size), sizeof(int));
-		swap_endianness(&file_size);
-		//std::cout << file_size << std::endl;
+    if(file){
+        file.read(reinterpret_cast<char*>(&file_type), 2);
+        swap_endianness(&file_type);
+        //std::cout << file_type << std::endl;
+        file.seekg(2, std::ios::cur);
+        file.read(reinterpret_cast<char*>(&file_size), sizeof(int));
+        swap_endianness(&file_size);
+        //std::cout << file_size << std::endl;
 
-		file.seekg(6+64, std::ios::beg);
-		int offset = 0;
-		while (offset < file_size) {
-			unsigned char header;
-			file.read(reinterpret_cast<char*>(&header), sizeof(char));
-			++offset;
+        file.seekg(6+64, std::ios::beg);
+        int offset = 0;
+        while (offset < file_size) {
+            unsigned char header;
+            file.read(reinterpret_cast<char*>(&header), sizeof(char));
+            ++offset;
 
-			unsigned short encoded;
-			unsigned char unencoded;
-			for (unsigned int mask = 0x80; mask !=  0; mask >>= 1) {
-				if (header & mask) {
-					file.read(reinterpret_cast<char*>(&encoded), sizeof(short));
-					offset += 2;
-					swap_endianness(&encoded);
+            unsigned short encoded;
+            unsigned char unencoded;
+            for (unsigned int mask = 0x80; mask !=  0; mask >>= 1) {
+                if (header & mask) {
+                    file.read(reinterpret_cast<char*>(&encoded), sizeof(short));
+                    offset += 2;
+                    swap_endianness(&encoded);
 
-					unsigned int count = 0x22 - ((encoded & 0xf800) >> 11);
-					unsigned int copy_source = encoded & 0x7ff;
+                    unsigned int count = 0x22 - ((encoded & 0xf800) >> 11);
+                    unsigned int copy_source = encoded & 0x7ff;
 
-					auto copy_from = extracted.end() - copy_source;
+                    auto copy_from = extracted.end() - copy_source;
                     std::vector<unsigned char> new_bytes;
                     if (copy_from + count <= extracted.end()) {
                         new_bytes.assign(copy_from, copy_from + count);
                     } else { 
-					    new_bytes.assign(copy_from, extracted.end());
+                        new_bytes.assign(copy_from, extracted.end());
                         int overlapped_bytes = count - copy_source;
                         for (int i = 0; i < overlapped_bytes; ++i) {
                             new_bytes.push_back(new_bytes[i]);
                         }
                     }
 
-					extracted.insert(extracted.end(), new_bytes.begin(),
-									 new_bytes.end());
+                    extracted.insert(extracted.end(), new_bytes.begin(),
+                                     new_bytes.end());
 
 
-				} else {
-					file.read(reinterpret_cast<char*>(&unencoded), sizeof(char));
-					++offset;
-					swap_endianness(&unencoded);
-					extracted.push_back(unencoded);
-				}
+                } else {
+                    file.read(reinterpret_cast<char*>(&unencoded), sizeof(char));
+                    ++offset;
+                    swap_endianness(&unencoded);
+                    extracted.push_back(unencoded);
+                }
                 if (offset >= file_size) {
                     break;
                 }
-			}
-		}
-	}
+            }
+        }
+    }
     return extracted;
 };
 
@@ -84,22 +84,22 @@ std::vector<unsigned char> readTestFile(std::ifstream& file) {
 }
 
 int main() {
-	std::ifstream file("MINDSCAP", std::ifstream::binary);
+    std::ifstream file("MINDSCAP", std::ifstream::binary);
     std::vector<unsigned char> extracted = extractFile(file);
-	//std::cout << "extracted size: " << extracted.size() << std::endl;
+    //std::cout << "extracted size: " << extracted.size() << std::endl;
 
-	std::ifstream testFile("mindscap_extract_1.bin", std::ifstream::binary);
+    std::ifstream testFile("mindscap_extract_1.bin", std::ifstream::binary);
     std::vector<unsigned char> test = readTestFile(testFile);
-	//std::cout << "test extracted size: " << test.size() << std::endl;
+    //std::cout << "test extracted size: " << test.size() << std::endl;
 
        /*
     int counter = 0;
     std::cout << std::hex << counter << "\t";
-	for (auto& i : extracted) {
+    for (auto& i : extracted) {
 
         if (i < 0x10)
             std::cout << 0;
-		std::cout << std::hex << +i << " ";
+        std::cout << std::hex << +i << " ";
         ++counter;
         if (counter % 4 == 0) {
             std::cout << " ";
@@ -111,7 +111,7 @@ int main() {
 
         }
 
-	}
+    }
     //*/
-	return 0;
+    return 0;
 }
