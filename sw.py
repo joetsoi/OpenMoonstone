9fd536066c28476ebfc322d73dd69601716b321e
joetsoi <joetsoi@users.noreply.github.com>
2017-10-02 00:03:11 +0100

    sw.py "starts with"
    small utitlity that takes a byte sequence and a file glob and returns the files that start with that byte sequence.
    
    Useful when trying to figure out what file a raw memory dump is.

---
+++ sw.py
@@ -0,0 +1,23 @@
+from codecs import decode
+from glob import glob
+import os, sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: view.arg <byte sequence> <filename>")
+        sys.exit()
+    #file_path=os.path.join(os.path.dirname(os.path.realpath(__file__)),
+    #                       sys.argv[2])
+    search_pattern = decode(sys.argv[1], 'hex')
+    num_chars = len(search_pattern)
+
+    for file_path in glob(sys.argv[2]):
+        with open(file_path, 'rb') as f:
+            data = f.read(num_chars)
+
+        if data.startswith(search_pattern):
+            print('match: {0}'.format(file_path))
+        else:
+            pass
+            #print('no match')
+
