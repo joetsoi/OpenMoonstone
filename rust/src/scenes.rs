4fd2f844f33c4c63b742516249e13858895bd8e8
Joe Tsoi <joetsoi@users.noreply.github.com>
2019-01-20 01:27:58 +0000

    mv menu.rs -> main_menu.rs

--- rust/src/scenes.rs
+++ rust/src/scenes.rs
@@ -5,12 +5,12 @@ use crate::input;
 
 pub mod encounter;
 pub mod main;
-pub mod menu;
+pub mod main_menu;
 pub mod transition;
 
 pub use self::encounter::EncounterScene;
 pub use self::main::MainScene;
-pub use self::menu::MainMenuScene;
+pub use self::main_menu::MainMenuScene;
 pub use self::transition::Fade;
 
 pub type FSceneSwitch = scene::SceneSwitch<game::Game, input::InputEvent>;
