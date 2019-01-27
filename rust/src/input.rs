adab9fbed47df0b30e09a37d3e8fa36a4b30f890
Joe Tsoi <joetsoi@users.noreply.github.com>
2019-01-27 01:40:30 +0000

    update Keycode -> KeyCode for ggez v0.5

--- rust/src/input.rs
+++ rust/src/input.rs
@@ -1,5 +1,6 @@
 //! Typedefs for input shortcuts.
 use ggez::event::*;
+use ggez::input::keyboard::KeyCode;
 use ggez_goodies::input;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -32,27 +33,27 @@ pub type InputState = input::InputState<Axis, Button>;
 /// Create the default keybindings for our input state.
 pub fn create_input_binding() -> input::InputBinding<Axis, Button> {
     input::InputBinding::new()
-        .bind_key_to_axis(Keycode::Up, Axis::Vert1, false)
-        .bind_key_to_axis(Keycode::Down, Axis::Vert1, true)
-        .bind_key_to_axis(Keycode::Left, Axis::Horz1, false)
-        .bind_key_to_axis(Keycode::Right, Axis::Horz1, true)
-        .bind_key_to_button(Keycode::Space, Button::Fire1)
+        .bind_key_to_axis(KeyCode::Up, Axis::Vert1, false)
+        .bind_key_to_axis(KeyCode::Down, Axis::Vert1, true)
+        .bind_key_to_axis(KeyCode::Left, Axis::Horz1, false)
+        .bind_key_to_axis(KeyCode::Right, Axis::Horz1, true)
+        .bind_key_to_button(KeyCode::Space, Button::Fire1)
         // player 2
-        .bind_key_to_axis(Keycode::W, Axis::Vert2, false)
-        .bind_key_to_axis(Keycode::S, Axis::Vert2, true)
-        .bind_key_to_axis(Keycode::A, Axis::Horz2, false)
-        .bind_key_to_axis(Keycode::D, Axis::Horz2, true)
-        .bind_key_to_button(Keycode::LCtrl, Button::Fire2)
+        .bind_key_to_axis(KeyCode::W, Axis::Vert2, false)
+        .bind_key_to_axis(KeyCode::S, Axis::Vert2, true)
+        .bind_key_to_axis(KeyCode::A, Axis::Horz2, false)
+        .bind_key_to_axis(KeyCode::D, Axis::Horz2, true)
+        .bind_key_to_button(KeyCode::LControl, Button::Fire2)
         //player 3
-        .bind_key_to_axis(Keycode::I, Axis::Vert3, false)
-        .bind_key_to_axis(Keycode::K, Axis::Vert3, true)
-        .bind_key_to_axis(Keycode::J, Axis::Horz3, false)
-        .bind_key_to_axis(Keycode::L, Axis::Horz3, true)
-        .bind_key_to_button(Keycode::G, Button::Fire3)
+        .bind_key_to_axis(KeyCode::I, Axis::Vert3, false)
+        .bind_key_to_axis(KeyCode::K, Axis::Vert3, true)
+        .bind_key_to_axis(KeyCode::J, Axis::Horz3, false)
+        .bind_key_to_axis(KeyCode::L, Axis::Horz3, true)
+        .bind_key_to_button(KeyCode::G, Button::Fire3)
         //player 4
-        .bind_key_to_axis(Keycode::Kp8, Axis::Vert4, false)
-        .bind_key_to_axis(Keycode::Kp5, Axis::Vert4, true)
-        .bind_key_to_axis(Keycode::Kp4, Axis::Horz4, false)
-        .bind_key_to_axis(Keycode::Kp6, Axis::Horz4, true)
-        .bind_key_to_button(Keycode::KpEnter, Button::Fire4)
+        .bind_key_to_axis(KeyCode::Numpad8, Axis::Vert4, false)
+        .bind_key_to_axis(KeyCode::Numpad5, Axis::Vert4, true)
+        .bind_key_to_axis(KeyCode::Numpad4, Axis::Horz4, false)
+        .bind_key_to_axis(KeyCode::Numpad6, Axis::Horz4, true)
+        .bind_key_to_button(KeyCode::NumpadEnter, Button::Fire4)
 }
