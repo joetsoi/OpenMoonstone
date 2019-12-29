ca38876ac1799159da47ac4eee446b9472b0808f
Joe Tsoi <joetsoi@users.noreply.github.com>
2019-12-29 20:05:59 +0000

    Add test entity to map scene
    Add the drawing code from encounter.rs to map.rs. This allows drawing of
    entities on the map screen
    
    This code should be refactored so both scenes use the same code, perhaps
    an DrawableEntities trait.

--- rust/src/scenes/map.rs
+++ rust/src/scenes/map.rs
@@ -1,3 +1,4 @@
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
@@ -7,17 +8,21 @@ use ggez::nalgebra::{Point2, Vector2};
 use ggez::{graphics, Context, GameResult};
 use ggez_goodies::scene::{Scene, SceneSwitch};
 use serde_derive::{Deserialize, Serialize};
+use specs::world::{Builder, Index};
 use specs::{Dispatcher, DispatcherBuilder, Join, World, WorldExt};
 use warmy::{SimpleKey, Store, StoreErrorOr};
 
 use loadable_yaml_macro_derive::LoadableYaml;
 
+use crate::animation::Image as SpriteImage;
+use crate::animation::Sprite;
 // TODO: move these components to common module out of combat
-use crate::combat::components::{Controller, Draw, Palette, Position, Velocity};
+use crate::combat::components::{Controller, Draw, Facing, Palette, Position, Velocity};
 use crate::combat::systems::Movement;
 use crate::error::LoadError;
 use crate::game::{Game, SceneState};
 use crate::input::{Axis, Button, InputEvent};
+use crate::objects::TextureAtlas;
 use crate::piv::{palette_swap, Colour, PivImage};
 use crate::scenes::FSceneSwitch;
 use crate::text::Image;
@@ -82,6 +87,8 @@ pub struct MapScene<'a> {
     pub background: Vec<graphics::Image>,
     background_frame: u32,
     current_background_image: usize,
+
+    pub palette: Vec<Colour>,
 }
 
 impl<'a> MapScene<'a> {
@@ -137,18 +144,35 @@ impl<'a> MapScene<'a> {
 
         piv.palette[21..24].rotate_right(1);
 
-        // world
-        //     .create_entity()
+        let mut specs_world = Self::build_world();
 
-        //     .with(Position { 10, 10 })
+        let sprite_res = store
+            .get::<Sprite>(&SimpleKey::from(format!("/{}.yaml", "mi")), ctx)
+            // TODO fix error handling, make this ?
+            .expect("error getting sprite in build entity");
+        let sprite = sprite_res.borrow();
+        specs_world
+            .create_entity()
+            .with(Position { x: 10, y: 10 })
+            .with(Velocity {
+                ..Default::default()
+            })
+            .with(Draw {
+                frame: sprite.animations["blue_knight"].frames[0].clone(),
+                animation: "blue_knight".to_string(),
+                resource_name: "mi".to_string(),
+                direction: Facing::default(),
+            })
+            .build();
 
         Ok(Self {
-            specs_world: Self::build_world(),
+            specs_world: specs_world,
             dispatcher: Self::build_dispatcher(),
             map_data,
             background,
             background_frame: 0,
             current_background_image: 0,
+            palette: piv.palette.to_vec(),
         })
     }
 
@@ -176,6 +200,7 @@ impl<'a> Scene<Game, InputEvent> for MapScene<'a> {
     }
 
     fn draw(&mut self, game: &mut Game, ctx: &mut Context) -> GameResult<()> {
+        let screen_origin = Point2::new(0.0, 0.0);
         self.draw_background_map(game, ctx);
 
         let palette_storage = self.specs_world.read_storage::<Palette>();
@@ -184,7 +209,43 @@ impl<'a> Scene<Game, InputEvent> for MapScene<'a> {
         let draw_storage = self.specs_world.read_storage::<Draw>();
         let entities = self.specs_world.entities();
         for (position, draw, entity) in (&position_storage, &draw_storage, &entities).join() {
-            println!("test");
+            let images: Vec<&SpriteImage> = draw.frame.images.iter().collect();
+            for image in images {
+                let atlas = game
+                    .store
+                    .get::<TextureAtlas>(&SimpleKey::from(image.sheet.as_str()), ctx)
+                    // TODO fix error handling, make this ?
+                    .expect("error loading texture atlas when drawing");
+                let atlas_dimension = atlas.borrow().image.width as u32;
+                let ggez_image = match game.images.entry(image.sheet.clone()) {
+                    Occupied(i) => i.into_mut(),
+                    Vacant(i) => i.insert(
+                        graphics::Image::from_rgba8(
+                            ctx,
+                            atlas_dimension as u16,
+                            atlas_dimension as u16,
+                            &atlas.borrow().image.to_rgba8(&self.palette),
+                        )
+                        .unwrap(),
+                    ),
+                };
+                let rect = atlas.borrow().rects[image.image];
+                let texture_size = atlas.borrow().image.width as f32;
+                let draw_params = graphics::DrawParam::default()
+                    .src(graphics::Rect {
+                        x: rect.x as f32 / texture_size,
+                        y: rect.y as f32 / texture_size,
+                        w: rect.w as f32 / texture_size,
+                        h: rect.h as f32 / texture_size,
+                    })
+                    .dest(Point2::new(
+                        (position.x as i32 + (draw.direction as i32 * image.x)) as f32 * 3.0,
+                        (position.y as i32 + image.y) as f32 * 3.0,
+                    ))
+                    .scale(Vector2::new((draw.direction as i32 * 3) as f32, 3.0));
+
+                graphics::draw(ctx, ggez_image, draw_params)?;
+            }
         }
         Ok(())
     }
