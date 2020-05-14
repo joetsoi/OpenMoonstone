0e1f22d33411df5059fe0c6ec6ca3f68d2a5608a
Joe Tsoi <joetsoi@users.noreply.github.com>
2020-05-14 21:10:21 +0100

    replace custom loads with derive(LoadableYaml/Ron)

--- rust/src/combat/damage.rs
+++ rust/src/combat/damage.rs
@@ -2,34 +2,16 @@ use std::collections::HashMap;
 
 use ggez::{filesystem, Context};
 use serde_yaml::Value;
+use serde_derive::{Deserialize, Serialize};
 use warmy;
+
+use loadable_macro_derive::{LoadableRon, LoadableYaml};
 
 use crate::error::LoadError;
 use crate::manager::GameYaml;
 
 type DamageTable = HashMap<String, u32>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize, LoadableYaml, LoadableRon)]
+#[serde(transparent)]
 pub struct DamageTables(pub HashMap<String, DamageTable>);
-
-impl warmy::Load<Context, warmy::SimpleKey> for DamageTables {
-    type Error = LoadError<GameYaml>;
-
-    fn load(
-        key: warmy::SimpleKey,
-        _store: &mut warmy::Storage<ggez::Context, warmy::SimpleKey>,
-        ctx: &mut ggez::Context,
-    ) -> Result<warmy::Loaded<Self, warmy::SimpleKey>, Self::Error> {
-        match key {
-            warmy::SimpleKey::Logical(key) => {
-                let file = filesystem::open(ctx, key)?;
-                let yaml: Value = serde_yaml::from_reader(file)?;
-
-                Ok(warmy::Loaded::from(DamageTables(serde_yaml::from_value(
-                    yaml,
-                )?)))
-            }
-            warmy::SimpleKey::Path(_) => return Err(LoadError::PathLoadNotImplemented),
-        }
-    }
-}
--- rust/src/palette.rs
+++ rust/src/palette.rs
@@ -1,34 +1,15 @@
 use std::collections::HashMap;
 
 use ggez::{filesystem, Context};
+use serde_derive::{Deserialize, Serialize};
 use serde_yaml::Value;
 use warmy;
+
+use loadable_macro_derive::{LoadableRon, LoadableYaml};
 
 use crate::error::LoadError;
 use crate::manager::GameYaml;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize, LoadableYaml, LoadableRon)]
+#[serde(transparent)]
 pub struct PaletteSwaps(pub HashMap<String, Vec<u16>>);
-
-// TODO: change this to a macro along with DamageTables
-impl warmy::Load<Context, warmy::SimpleKey> for PaletteSwaps {
-    type Error = LoadError<GameYaml>;
-
-    fn load(
-        key: warmy::SimpleKey,
-        _store: &mut warmy::Storage<ggez::Context, warmy::SimpleKey>,
-        ctx: &mut ggez::Context,
-    ) -> Result<warmy::Loaded<Self, warmy::SimpleKey>, Self::Error> {
-        match key {
-            warmy::SimpleKey::Logical(key) => {
-                let file = filesystem::open(ctx, key.as_str())?;
-                let yaml: Value = serde_yaml::from_reader(file)?;
-
-                Ok(warmy::Loaded::from(PaletteSwaps(serde_yaml::from_value(
-                    yaml,
-                )?)))
-            }
-            warmy::SimpleKey::Path(_) => return Err(LoadError::PathLoadNotImplemented),
-        }
-    }
-}
