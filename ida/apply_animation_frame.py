e93efbf12957718587ff9fc8fa31a877e222e307
Joe Tsoi <joetsoi@users.noreply.github.com>
2020-06-21 18:12:41 +0100

    apply_animation_frame now usable in ida without the debugger running.
    Currently handles applying AnimationFrame and EndOfAnimation structs,
    handling of functions can happen later.

--- ida/apply_animation_frame.py
+++ ida/apply_animation_frame.py
@@ -2,13 +2,31 @@ import sark
 import idaapi
 import idautils
 
-anim = sark.structure.get_struct('AnimationFrame')
-while idaapi.is_debugger_on():
 
-    dataseg =  sark.Segment(name='dataseg').ea
-    anim_offset = idaapi.get_word(sark.Line(ea=dataseg + idautils.cpu.di + 2).ea)
-    anim_addr = dataseg + anim_offset
-    idaapi.doStruct(anim_addr, 6, anim)
-    idaapi.jumpto(sark.Segment(name='dataseg').ea + anim_offset)
-    idaapi.continue_process()
-    idaapi.wait_for_next_event(2, 10000)
+anim = sark.structure.get_struct('TroggSpearImage')
+end_of_frame = sark.structure.get_struct("EndOfAnimFrame")
+dataseg =  sark.Segment(name='dataseg').ea
+# anim_offset = idaapi.get_word(sark.Line(ea=dataseg + idautils.cpu.di + 2).ea)
+current_position = sark.Line().ea
+# current_byte = idaapi.get_byte(current_position)
+
+done = False
+
+while not done:
+    current_byte = idaapi.get_byte(current_position)
+    if current_byte == 0xff:
+        print("applying EndOfAnimFrame")
+        idaapi.doStruct(current_position, 2, end_of_frame)
+        next_byte = idaapi.get_byte(current_position + 1)
+        if next_byte == 0xff:
+            done = True
+        current_position += 2
+    elif current_byte < 0x80:
+        # print(current_byte)
+        print("applying AnimationFrame")
+        test = idaapi.doStruct(current_position, 6, anim)
+        # print(test)
+        current_position += 6
+        # print(hex(current_position-dataseg))
+    else:
+        done = True
