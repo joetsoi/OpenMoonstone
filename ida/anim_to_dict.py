1a3cae45df8623cff80222f81d8e1872c9cdc3b6
Joe <joetsoi@users.noreply.github.com>
2021-01-31 18:52:27 +0000

    update anim_to_dict to handle balok animations

--- ida/anim_to_dict.py
+++ ida/anim_to_dict.py
@@ -19,12 +19,27 @@ def byte_to_sign(value):
     return value
 
 
+KNIGHT = 'AnimationFrame'
+TROGG_SPEAR = 'TroggSpearImage'
+BALOK = 'BalokImage'
+
 spritesheets = {
-    0: 'kn1.ob',
-    4: 'kn2.ob',
-    8: 'kn3.ob',
-    12: 'kn4.ob',
-    16: 'kn4.ob',
+    KNIGHT: {
+        0: 'kn1.ob',
+        4: 'kn2.ob',
+        8: 'kn3.ob',
+        12: 'kn4.ob',
+        16: 'kn4.ob',
+    },
+    TROGG_SPEAR: {
+        0: 'troggsp1.cel',
+        4: 'troggsp2.cel',
+    },
+    BALOK: {
+        0: 'balok1.cel',
+        4: 'balok2.cel',
+        8: 'balok3.cel',
+    },
 }
 
 collide_type = {
@@ -32,31 +47,43 @@ collide_type = {
     1: 'Collidee',
     2: 'Collider',
     16: 'Vm',
+    32: 'WeaponHand',
+    64: 'UpdateEdge',
+    65: 'Collidee',
+    66: 'Collider',
     128: 'Blood',
     144: 'BloodStain',
 }
 
+animations = [
+    KNIGHT,
+    TROGG_SPEAR,
+    BALOK,
+]
 
 line = sark.Line()
 next_line = True
-while next_line:
-    if line.disasm.startswith("AnimationFrame"):
 
+
+while next_line:
+    image_type = line.disasm.split()[0]
+    if image_type in animations:
         sprite, img_num, y, collide, x = unpack('<4BH', line.bytes)
         y = byte_to_sign(y)
         x = hex_to_sign(x)
-        sprite = spritesheets[sprite]
+        sprite = spritesheets[image_type][sprite]
         collide = collide_type[collide]
 
         #test = "ImagePosition('{}', {}, {}, {}, {}),".format(sprite, img_num, y, x, collide)
-        test = "  - {{sheet: {}, image: {}, y: {}, x: {}, image_type:{}}}".format(sprite, img_num, y, x, collide)
+        test = '  (sheet: "{}", image: {}, y: {}, x: {}, image_type:{}),'.format(sprite, img_num, y, x, collide)
         print test
         line = line.next
     elif line.disasm.startswith('EndOfAnimFrame <0FFh, 0>'):
         line = line.next
 
         # print '),'
-        print '- images:'
+        print '],'
+        print 'images: ['
     elif line.disasm.startswith('EndOfAnimFrame <0FFh, 0FFh>'):
         next_line = False
     else:
